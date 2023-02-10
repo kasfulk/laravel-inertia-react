4fdc3073257c4c54d841c52152b7d12e9c0f441a
Kasjful Kurniawan <kasful.bpp@gmail.com>
2023-02-10 14:58:56 +0800

    chore: init eslint prettier

--- resources/js/Components/Header.tsx
+++ resources/js/Components/Header.tsx
@@ -1,123 +1,147 @@
-import { createStyles, Header, Menu, Group, Center, Burger, Container } from '@mantine/core';
+import {
+    createStyles,
+    Header,
+    Menu,
+    Group,
+    Center,
+    Burger,
+    Container,
+} from '@mantine/core';
 import { Link } from '@inertiajs/inertia-react';
 import { useDisclosure } from '@mantine/hooks';
 import { IconChevronDown } from '@tabler/icons';
 import { MantineLogo } from '@mantine/ds';
 
 const useStyles = createStyles((theme) => ({
-  header: {
-    backgroundColor: theme.fn.variant({ variant: 'filled', color: theme.primaryColor }).background,
-    borderBottom: 0,
-  },
-
-  iconSize: {
-    height:12
-  },
-
-  inner: {
-    height: 56,
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  },
-
-  links: {
-    [theme.fn.smallerThan('sm')]: {
-      display: 'none',
+    header: {
+        backgroundColor: theme.fn.variant({
+            variant: 'filled',
+            color: theme.primaryColor,
+        }).background,
+        borderBottom: 0,
     },
-  },
 
-  burger: {
-    [theme.fn.largerThan('sm')]: {
-      display: 'none',
+    iconSize: {
+        height: 12,
     },
-  },
 
-  link: {
-    display: 'block',
-    lineHeight: 1,
-    padding: '8px 12px',
-    borderRadius: theme.radius.sm,
-    textDecoration: 'none',
-    color: theme.white,
-    fontSize: theme.fontSizes.sm,
-    fontWeight: 500,
-
-    '&:hover': {
-      backgroundColor: theme.fn.lighten(
-        theme.fn.variant({ variant: 'filled', color: theme.primaryColor }).background!,
-        0.1
-      ),
+    inner: {
+        height: 56,
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
     },
-  },
 
-  linkLabel: {
-    marginRight: 5,
-  },
+    links: {
+        [theme.fn.smallerThan('sm')]: {
+            display: 'none',
+        },
+    },
+
+    burger: {
+        [theme.fn.largerThan('sm')]: {
+            display: 'none',
+        },
+    },
+
+    link: {
+        display: 'block',
+        lineHeight: 1,
+        padding: '8px 12px',
+        borderRadius: theme.radius.sm,
+        textDecoration: 'none',
+        color: theme.white,
+        fontSize: theme.fontSizes.sm,
+        fontWeight: 500,
+
+        '&:hover': {
+            backgroundColor: theme.fn.lighten(
+                theme.fn.variant({
+                    variant: 'filled',
+                    color: theme.primaryColor,
+                }).background!,
+                0.1,
+            ),
+        },
+    },
+
+    linkLabel: {
+        marginRight: 5,
+    },
 }));
 
 export interface HeaderSearchProps {
-  links: { link: string; label: string; links?: { link: string; label: string }[] }[];
+    links: {
+        link: string;
+        label: string;
+        links?: { link: string; label: string }[];
+    }[];
 }
 
 export function HeaderWeb({ links }: HeaderSearchProps) {
-  const [opened, { toggle }] = useDisclosure(false);
-  const { classes } = useStyles();
+    const [opened, { toggle }] = useDisclosure(false);
+    const { classes } = useStyles();
 
-  const items = links.map((link) => {
-    const menuItems = link.links?.map((item) => (
-      <Menu.Item key={item.link}>{item.label}</Menu.Item>
-    ));
+    const items = links.map((link) => {
+        const menuItems = link.links?.map((item) => (
+            <Menu.Item key={item.link}>{item.label}</Menu.Item>
+        ));
 
-    if (menuItems) {
-      return (
-        <Menu key={link.label} trigger="hover" exitTransitionDuration={0}>
-          <Menu.Target>
-            <Link
-              href={link.link}
-              className={classes.link}
-              onClick={(event) => event.preventDefault()}
+        if (menuItems) {
+            return (
+                <Menu
+                    key={link.label}
+                    trigger="hover"
+                    exitTransitionDuration={0}
+                >
+                    <Menu.Target>
+                        <Link
+                            href={link.link}
+                            className={classes.link}
+                            onClick={(event) => event.preventDefault()}
+                        >
+                            <Center>
+                                <span className={classes.linkLabel}>
+                                    {link.label}
+                                </span>
+                                <IconChevronDown size={12} />
+                            </Center>
+                        </Link>
+                    </Menu.Target>
+                    <Menu.Dropdown>{menuItems}</Menu.Dropdown>
+                </Menu>
+            );
+        }
+
+        return (
+            <a
+                key={link.label}
+                href={link.link}
+                className={classes.link}
+                onClick={(event) => event.preventDefault()}
             >
-              <Center>
-                <span className={classes.linkLabel}>{link.label}</span>
-                <IconChevronDown/>
-              </Center>
-            </Link>
-          </Menu.Target>
-          <Menu.Dropdown>{menuItems}</Menu.Dropdown>
-        </Menu>
-      );
-    }
+                {link.label}
+            </a>
+        );
+    });
 
     return (
-      <a
-        key={link.label}
-        href={link.link}
-        className={classes.link}
-        onClick={(event) => event.preventDefault()}
-      >
-        {link.label}
-      </a>
+        <Header height={56} className={classes.header}>
+            <Container>
+                <div className={classes.inner}>
+                    <MantineLogo size={28} inverted />
+                    <Group spacing={5} className={classes.links}>
+                        {items}
+                    </Group>
+                    <Burger
+                        opened={opened}
+                        onClick={toggle}
+                        className={classes.burger}
+                        size="sm"
+                        color="#fff"
+                    />
+                </div>
+            </Container>
+        </Header>
     );
-  });
-
-  return (
-    <Header height={56} className={classes.header}>
-      <Container>
-        <div className={classes.inner}>
-          <MantineLogo size={28} inverted />
-          <Group spacing={5} className={classes.links}>
-            {items}
-          </Group>
-          <Burger
-            opened={opened}
-            onClick={toggle}
-            className={classes.burger}
-            size="sm"
-            color="#fff"
-          />
-        </div>
-      </Container>
-    </Header>
-  );
 }
